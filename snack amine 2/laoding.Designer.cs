e021e6ddee0cee8def33ef3f7debd0e6f939b171
marwan <marouanebenmhand2@gmail.com>
2023-11-26 12:43:29 +0100

    fixed timer

--- snack amine 2/laoding.Designer.cs
+++ snack amine 2/laoding.Designer.cs
@@ -35,6 +35,7 @@
             // 
             // timer1
             // 
+            this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
             // 
             // pbar
@@ -42,21 +43,21 @@
             this.pbar.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
             this.pbar.AnimationSpeed = 500;
             this.pbar.BackColor = System.Drawing.Color.Transparent;
-            this.pbar.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold);
+            this.pbar.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.pbar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.pbar.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pbar.InnerColor = System.Drawing.Color.Empty;
             this.pbar.InnerMargin = 2;
             this.pbar.InnerWidth = -1;
-            this.pbar.Location = new System.Drawing.Point(150, 71);
+            this.pbar.Location = new System.Drawing.Point(219, 62);
             this.pbar.MarqueeAnimationSpeed = 2000;
             this.pbar.Name = "pbar";
-            this.pbar.OuterColor = System.Drawing.Color.Gray;
+            this.pbar.OuterColor = System.Drawing.Color.Transparent;
             this.pbar.OuterMargin = -25;
             this.pbar.OuterWidth = 26;
-            this.pbar.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pbar.ProgressColor = System.Drawing.Color.Cyan;
             this.pbar.ProgressWidth = 25;
             this.pbar.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
-            this.pbar.Size = new System.Drawing.Size(320, 320);
+            this.pbar.Size = new System.Drawing.Size(295, 290);
             this.pbar.StartAngle = 270;
             this.pbar.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
             this.pbar.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
@@ -65,7 +66,6 @@
             this.pbar.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
             this.pbar.SuperscriptText = "°C";
             this.pbar.TabIndex = 0;
-            this.pbar.Text = "circularProgressBar1";
             this.pbar.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
             this.pbar.Value = 68;
             // 
@@ -73,6 +73,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.pbar);
             this.Name = "laoding";
